4ca9d723d53dc2ebeb2d6477529bb4f513f33d55
Tom <tomruiz2611@gmail.com>
2022-04-04 21:47:54 +0200

    fix logo navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ function NavbarComponent(){
     return(
         <Navbar className="nav" variant="light" fixed='top'>
           <Container>
-          <Navbar.Brand href="/" className="nav__title"><img className="home-logo" src={picto} alt="picto"/></Navbar.Brand>
+          <Navbar.Brand href="/" className="nav__title"><img src={picto} alt="picto"/></Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link className="nav__link" href="./Events">Evènements</Nav.Link>
           </Nav>
